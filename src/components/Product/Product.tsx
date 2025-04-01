7ed34d64af46403cea8a47cc4c20d7dbebf449bb
aleksshymko <aleksshymko@gmail.com>
2025-04-02 01:52:39 +0200

    add homework11

--- src/components/Product/Product.tsx
+++ src/components/Product/Product.tsx
@@ -1,7 +1,14 @@
 import { ProductProps } from "./types";
 import "./styles.css";
+import { useEffect } from "react";
 
 function Product({ productName, productPrice }: ProductProps) {
+  useEffect(()=>{
+    return ()=>{
+      console.log('Component will unmount');
+    }
+  }, []);
+  
   return (
     <div className="product-container">
       <div> Product: {productName} </div>
