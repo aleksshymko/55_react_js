ebe70814ef7fd73e203bfdff0ccf99610587e868
aleksshymko <aleksshymko@gmail.com>
2025-04-07 23:18:25 +0200

    consultation06

--- src/consultations/Consultation06/Consultation06.tsx
+++ src/consultations/Consultation06/Consultation06.tsx
@@ -1,11 +1,47 @@
-import { Consultation06Container, UserCard } from "./styles"
+import { useEffect, useState } from "react";
+import {
+  Avatar,
+  Consultation06Container,
+  UserCard,
+  UserInfo,
+  UserName,
+} from "./styles";
+import axios from "axios";
 
-function Consultation06 () {
-    return (
-        <Consultation06Container>
-            <UserCard></UserCard>
-        </Consultation06Container>
-    )
+function Consultation06() {
+  const USER_URL: string = "https://randomuser.me/api/";
+
+  const [userData, setUserData] = useState<any>(undefined);
+  console.log(userData);
+
+  const getUser = async () => {
+    try {
+      const response = await axios.get(USER_URL);
+      setUserData(response.data.results[0]);
+    } catch (error: any) {
+    } finally {
+    }
+  };
+
+  useEffect(() => {
+    getUser();
+  }, []);
+
+  return (
+    <Consultation06Container>
+      {userData && (
+        <UserCard>
+          <Avatar src={userData?.picture?.large} alt="user avatar" />
+          <UserName>
+            Name:
+            {`${userData?.name?.title} ${userData?.name?.first} ${userData?.name?.last}`}
+          </UserName>
+          <UserInfo>Phone:{userData.phone}</UserInfo>
+          <UserInfo>Email:{userData.email}</UserInfo>
+        </UserCard>
+      )}
+    </Consultation06Container>
+  );
 }
 
-export default Consultation06
+export default Consultation06;
