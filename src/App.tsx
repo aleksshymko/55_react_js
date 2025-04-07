18198e93a0da35e7dca86f5296b30efc93e622c5
aleksshymko <aleksshymko@gmail.com>
2025-04-07 16:04:42 +0200

    update_conflict

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,4 @@
-//import Homework10 from "./homeworks/Homework10/Homework10";
-// import Lesson11 from "./lessons/Lesson11/Lesson11";
 // import Consultation05 from "./consultations/Consultation05/Consultation05";
-//import Homework11 from "./homeworks/Homework11/Homework11";
-// import Lesson12 from "./lessons/Lesson12/Lesson12";
-//import Homework12 from "./homeworks/Homework12/Homework12";
-//import Lesson13 from "./lessons/Lesson13/Lesson13";
 //import Lesson14 from "./lessons/Lesson14/Lesson14";
 import Homework14 from "./homeworks/Homework14/Homework14";
 import GlobalStyles from "./styles/GlobalStyles";
@@ -16,10 +10,8 @@ function App() {
   return (
     <>
       <GlobalStyles />
-      {/* <Task06 /> */}
-      <Homework12 />
-      {/* <Button/> */}
-      {/* <Lesson12 /> */}
+      <Homework14 />
+      {/* <Lesson14 /> */}
       {/* <Consultation05/> */}
     </>
   );
