394319e8d5df3a20f29070399e7e848744968e98
aleksshymko <aleksshymko@gmail.com>
2025-04-08 22:51:32 +0200

    cons06end

--- src/consultations/Consultation06/Consultation06.tsx
+++ src/consultations/Consultation06/Consultation06.tsx
@@ -7,20 +7,33 @@ import {
   UserName,
 } from "./styles";
 import axios from "axios";
+import Button from "../../components/Button/Button";
 
 function Consultation06() {
   const USER_URL: string = "https://randomuser.me/api/";
 
   const [userData, setUserData] = useState<any>(undefined);
   console.log(userData);
+  const [isTimerOn, setIsTimerOn] = useState<boolean>(false);
 
   const getUser = async () => {
-    try {
-      const response = await axios.get(USER_URL);
-      setUserData(response.data.results[0]);
-    } catch (error: any) {
-    } finally {
-    }
+    // if (isTimerOn) {
+    //   setIsTimerOn(false)
+    // } else {
+    //   setIsTimerOn(true)
+    // }
+    setIsTimerOn((prevValue)=>!prevValue)
+    const intervalID = setInterval (async() => {
+      if(isTimerOn) {
+        clearInterval(intervalID)
+      } else {
+      try {
+        const response = await axios.get(USER_URL);
+        setUserData(response.data.results[0]);
+      } catch (error: any) {
+      } finally {
+      }
+}, 5000)};
   };
 
   useEffect(() => {
@@ -38,6 +51,7 @@ function Consultation06() {
           </UserName>
           <UserInfo>Phone:{userData.phone}</UserInfo>
           <UserInfo>Email:{userData.email}</UserInfo>
+          <Button name={isTimerOn ? 'CANCEL RANDOM USER' : "GET RANDOM USER"} onClick={getUser} danger={isTimerOn}/>
         </UserCard>
       )}
     </Consultation06Container>
