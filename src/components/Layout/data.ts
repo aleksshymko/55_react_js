547c952d919f01247a5f2c4763c96d88cf6f4229
aleksshymko <aleksshymko@gmail.com>
2025-04-10 01:49:10 +0200

    homework17

--- src/components/Layout/data.ts
+++ src/components/Layout/data.ts
@@ -5,4 +5,5 @@ export const navLinksData: NavLinkObj[] = [
   { to: "/about", linkName: "About" },
   { to: "/course", linkName: "Course" },
   { to: "/users", linkName: "User" },
+  { to: "/clients", linkName: "Clients" },
 ];
